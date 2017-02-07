fa07e68ec5a0902d8f852222bc46157abed968d6
Ugur Kodak <Ugur Kodak>
2017-02-07 13:37:04 -0500

    Initialized node app with express framework

---
+++ server.js
@@ -0,0 +1,13 @@
+let express = require("express");
+let server = express();
+let port = process.env.PORT || 5000;
+
+server.get("/", function(req, res)
+			  {
+					res.send("COMP313");
+			  });
+
+server.listen(port, function()
+				  {
+						console.log("Server is running.");
+				  });
