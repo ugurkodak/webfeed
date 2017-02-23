337dac1efdc7ec0d4c0a8f4a8c7412a0c819b692
Ugur Kodak <Ugur Kodak>
2017-02-23 16:55:00 -0500

    Testing CRUD with mLAb

--- server.js
+++ server.js
@@ -1,3 +1,47 @@
+//Database connection
+let mongoose = require("mongoose");
+let URI = "mongodb://webfeed_admin:12345@ds161059.mlab.com:61059/webfeed"
+mongoose.Promise = global.Promise; //Remove Promise library depreciation warning (Temporary)
+mongoose.connect(URI, function(error)
+    {
+	if(error)
+	    {
+		console.log("Connected to webfeed database");
+	    }
+    });
+let database = mongoose.connection;
+
+// animal model (animals collection) for testing CRUD aoperations -------------Temporary---------------
+let animalSchema = mongoose.Schema(
+    {
+	type: "string",
+	age: "string"
+	
+    });
+let animal = mongoose.model("animal", animalSchema);
+
+//Create
+/* let testAnimal = new animal(
+ *     {
+ *    	type: "Dog",
+ *    	age: "12"
+ *     });
+ * testAnimal.save(function(error)
+ *     {
+ *    	if(!error)
+ *    	    {
+ * 		console.log("testAnimal is added to database.");
+ *    	    };
+ *     });*/
+
+//Print all animal documents
+let query = animal.find({});
+query.exec(function(error, result)
+    {
+	console.log(result);
+    });
+//------------------------------------------------------------------Temporary------------------------
+
 //Server
 let express = require("express");
 let server = express();
@@ -11,20 +55,4 @@ server.get("/", function(req, res)
 server.listen(port, function()
     {
 	console.log("Server is running.");
-    });
-
-//Database connection
-let mongoose = require("mongoose");
-let URI = "mongodb://webfeed_admin:12345@ds161059.mlab.com:61059/webfeed"
-
-mongoose.connect(URI, function(error)
-    {
-	if(error)
-	    {
-		console.log("Error connectiong to database.");
-	    }
-	else
-	    {
-		console.log("Connected to MongoDB");
-	    }
     });
