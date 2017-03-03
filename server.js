fb5f1c6eb2041964b2bc9cecea38b0a77d233e08
Ugur Kodak <Ugur Kodak>
2017-03-02 19:21:09 -0500

    Print one popular tweet from top 5 trending topics.

--- server.js
+++ server.js
@@ -2,24 +2,31 @@ let express = require("express");
 let server = express();
 let port = process.env.PORT || 5000;
 
-let database = require("./database");
-let api = require("./api");
+let database = require(__dirname + "/database");
+let api = require(__dirname + "/api");
+
+server.use(express.static(__dirname + "/static"));
+server.set("views", __dirname + "/views");
+server.set("view engine", "ejs");
 
 //Testing twitter
-//Get one popular tweet from first and second trending.
-let popularTweets = [];
-for (let i = 0; i < 2; i++)
+//Get one popular tweet from 5 trending.
+let posts = [];
+for (let i = 0; i < 5; i++)
     {
 	api.twitter.getPopularTweet(i, function(tweet)
 	    {
-		popularTweets[i] = tweet.text
+		posts[i] = tweet.text
 	    });	
     }
 
-
 server.get("/", function(req, res)
     {
-	res.send(popularTweets[0] + "\n" + popularTweets[1]);
+	res.render("home",
+		   {
+		       title: "WebFeed",
+		       posts: posts
+		   });
     });
 
 server.listen(port, function()
